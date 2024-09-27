d20ab2ee087bdd04a155d90b287e59ee12f61d96
Eduardo Duda <edududa.pa@gmail.com>
2024-09-27 18:41:26 -0300

    Update Estoque.cpp

--- Estoque.cpp
+++ Estoque.cpp
@@ -1,47 +1,69 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-//Classe Carro
 
-//Terminar metodo main usando os construtores e função de venda.
+//Classe Carro
+//Terminar metodo main usando os construtores e metodo de venda.
 class Carro{
-    
+
+    public:
     int id;
-    string mar, placa;
-    double peso;
+    string mod;
+    double preco;
     bool sell;
     
-    cons(){
-        id=0;
+    Carro()
+    {
+        id=-1;
+        mod="";       
+        preco=0;        
         sell=false;
-        placa="";
-        peso=0;
         
     }
     
-    cons(int n, string mar, string placa, int peso){
-        this->id=id;
-        this->sell=sell;
-        this->placa=placa;
-        this->peso=peso;
+    Carro(int id, string mod, double preco)
+    {
+        this->id=id; 
+        this->mod=mod;   
+        this->preco=preco;
+
+        this->sell=false;
     }
     
     void buy(int id)
     {
-        if(this->sell==false)
-        {
-            sell=true;
-            cout<<"\nCarro vendido!";
-        }else
-        {
-            cout<<"\nCarro já vendido!";
-        }
+        char SN;
+        cout<<(!sell)?"\nCarro Disponível!":"\nCarro já vendido!";
+        cout<<"\n O carro já foi vendido?"; cin>>SN;
+        sell=(SN=='S'||SN=='s') ? true : false;
+        
     }
 };
 
-int main() {
-    
-    Cons
+vector<Carro> est;
+
+int main() 
+{
+    setlocale(LC_ALL, "portuguese");
+    int n;
+    cout<<"Armazenamento simples para carros, insira os dados dos carros antes de começar.";
+    cout<<"\nHá quantos carros no estoque?(id: 1-N) "; cin>> n;
+
+    for(int i=0; i<n; i++)
+    {
+
+        string m;
+        double price;
+        cout<<"\nInsira o modelo do carro "<<i+1<<": "; cin >> m;
+        cout<<"Insira o preço do carro "<<i+1<<": "; cin>>price;
+
+        est.push_back(Carro(i+1, m ,price));
+    }
+    cout<<"Dados preenchidos.";
+    while(bool v=1)
+    {
+        est.buy(4);//ERRO CRITICO, termina outro dia
+    }
 
     return 0;
 }
