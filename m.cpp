261ba27a50432688edd686d04b57e95f8bb9730d
Eduardo Duda <edududa.pa@gmail.com>
2026-09-03 18:10:15 -0300

    a
    a

---
+++ m.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n, m, k;
+    cin>>n>>m>>k;
+
+    int grafo[n][n];
+    
+    for(int i=0; i<m; i++)
+    {
+        int a, b, f, w;
+        cin>>a>>b>>f>>w;
+
+    }
+
+    return 0;
+}
